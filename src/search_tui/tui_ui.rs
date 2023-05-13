7eaf6a11f13c55a0452dbdee543533e398be1e18
Miika Alikirri <nykseli@protonmail.com>
2023-05-13 14:18:03 +0300

    Reorganise tui layout

--- src/search_tui/tui_ui.rs
+++ src/search_tui/tui_ui.rs
@@ -10,21 +10,7 @@ use unicode_width::UnicodeWidthStr;
 
 use super::tui_state::{InputMode, TuiState};
 
-pub fn ui<B: Backend>(f: &mut Frame<B>, app: &mut TuiState) {
-    let chunks = Layout::default()
-        .direction(Direction::Vertical)
-        .margin(2)
-        .constraints(
-            [
-                Constraint::Length(1),
-                Constraint::Length(3),
-                Constraint::Min(1),
-                Constraint::Length(3),
-            ]
-            .as_ref(),
-        )
-        .split(f.size());
-
+fn help_message_widget(app: &TuiState) -> Paragraph {
     let (msg, style) = match app.input_mode {
         InputMode::Select => (
             vec![
@@ -34,7 +20,7 @@ pub fn ui<B: Backend>(f: &mut Frame<B>, app: &mut TuiState) {
                 Span::styled("e", Style::default().add_modifier(Modifier::BOLD)),
                 Span::raw(" to start searching."),
             ],
-            Style::default(), // Style::default().add_modifier(Modifier::RAPID_BLINK),
+            Style::default(),
         ),
         InputMode::Search => (
             vec![
@@ -47,36 +33,24 @@ pub fn ui<B: Backend>(f: &mut Frame<B>, app: &mut TuiState) {
             Style::default(),
         ),
     };
+
     let mut text = Text::from(Spans::from(msg));
     text.patch_style(style);
-    let help_message = Paragraph::new(text);
-    f.render_widget(help_message, chunks[0]);
+    Paragraph::new(text)
+}
 
-    let input = Paragraph::new(app.input.as_ref())
+fn input_widget(app: &TuiState) -> Paragraph {
+    Paragraph::new(app.input.as_ref())
         .style(match app.input_mode {
             InputMode::Select => Style::default(),
             InputMode::Search => Style::default().fg(Color::Yellow),
         })
-        .block(Block::default().borders(Borders::ALL).title("Input"));
-    f.render_widget(input, chunks[1]);
-    match app.input_mode {
-        InputMode::Select =>
-            // Hide the cursor. `Frame` does this by default, so we don't need to do anything here
-            {}
+        .block(Block::default().borders(Borders::ALL).title("Input"))
+}
 
-        InputMode::Search => {
-            // Make the cursor visible and ask tui-rs to put it at the specified coordinates after rendering
-            f.set_cursor(
-                // Put cursor past the end of the input text
-                chunks[1].x + app.input.width() as u16 + 1,
-                // Move one line down, from the border to the input line
-                chunks[1].y + 1,
-            )
-        }
-    }
-
+fn paths_view_widget(app: &mut TuiState) -> List {
     // TODO: show name and optional description!
-    let messages: Vec<ListItem> = app
+    let paths: Vec<ListItem> = app
         .items
         .filter_paths(&app.input)
         .enumerate()
@@ -91,18 +65,62 @@ pub fn ui<B: Backend>(f: &mut Frame<B>, app: &mut TuiState) {
         .collect();
 
     // TODO: handle this more cleanly
-    app.filtered_len = messages.len();
+    app.filtered_len = paths.len();
 
-    let messages =
-        List::new(messages).block(Block::default().borders(Borders::ALL).title("Messages"));
-    f.render_widget(messages, chunks[2]);
+    List::new(paths).block(Block::default().borders(Borders::ALL).title("Paths"))
+}
+
+fn path_description_widget() -> Paragraph<'static> {
+    Paragraph::new("Super secert info!")
+        .block(Block::default().borders(Borders::all()).title("Info"))
+}
+
+pub fn ui<B: Backend>(f: &mut Frame<B>, app: &mut TuiState) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(2)
+        .constraints(
+            [
+                // Path description
+                Constraint::Length(3),
+                // List of paths
+                Constraint::Min(1),
+                // Input help message
+                Constraint::Length(1),
+                // Input
+                Constraint::Length(3),
+            ]
+            .as_ref(),
+        )
+        .split(f.size());
 
     // TODO: Display extra info about the path, split text based on the width of
     //       current terminal window
+    let description = path_description_widget();
+    f.render_widget(description, chunks[0]);
 
-    f.render_widget(
-        Paragraph::new("Super secert info!")
-            .block(Block::default().borders(Borders::all()).title("Info")),
-        chunks[3],
-    )
+    let paths = paths_view_widget(app);
+    f.render_widget(paths, chunks[1]);
+
+    let help_message = help_message_widget(app);
+    f.render_widget(help_message, chunks[2]);
+
+    match app.input_mode {
+        InputMode::Select =>
+            // Hide the cursor. `Frame` does this by default, so we don't need to do anything here
+            {}
+
+        InputMode::Search => {
+            // Make the cursor visible and ask tui-rs to put it at the specified coordinates after rendering
+            f.set_cursor(
+                // Put cursor past the end of the input text
+                chunks[3].x + app.input.width() as u16 + 1,
+                // Move one line down, from the border to the input line
+                chunks[3].y + 1,
+            )
+        }
+    }
+
+    let input = input_widget(app);
+    f.render_widget(input, chunks[3]);
 }
